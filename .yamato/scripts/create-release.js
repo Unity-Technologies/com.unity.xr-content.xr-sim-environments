b5aca5532b003bb74ec33f185d43250c3910ab51
Amy DiGiovanni <amyd@unity3d.com>
2022-05-19 14:08:40 -0700

    Add yamato job that creates tgz of package and adds it to a GitHub release

---
+++ .yamato/scripts/create-release.js
@@ -0,0 +1,36 @@
+const { Octokit } = require("@octokit/core");
+const fs = require("fs");
+
+const octokit = new Octokit();
+
+const repoUrl = process.env.GIT_REPOSITORY_URL;
+// Start after "git@github.com:" and omit ".git" from the end
+const repoPath = repoUrl.substring(15, repoUrl.length - 4);
+const [repoOwner, repoName] = repoPath.split("/");
+const tagName = process.env.GIT_TAG;
+const releaseDescription = 'XR Simulation Environments version ' + tagName;
+const tgzFileName = repoName + '-' + tagName + '.tgz';
+const tgzPath = 'upm-ci~/packages/' + tgzFileName;
+
+async function createRelease() {
+	const createReleaseResult = await octokit.request('POST /repos/{owner}/{repo}/releases', {
+		owner: repoOwner,
+		repo: repoName,
+		tag_name: tagName,
+		name: tagName,
+		body: releaseDescription,
+		draft: false,
+		prerelease: false,
+		generate_release_notes: false
+	});
+	
+	var data = fs.readFileSync(tgzPath);
+	const uploadURL = createReleaseResult.data.upload_url;
+	const uploadResult = await octokit.request('POST ' + uploadURL, {
+		name: tgzFileName,
+		label: tgzFileName,
+		data
+	});
+}
+
+createRelease();
